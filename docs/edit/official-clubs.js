fa8eacb3f0d7e0e646ff931a24f6a0555892ad3f
stuseed42-beep <stuseed42@gmail.com>
2026-02-08 15:37:34 +0900

    Update official clubs data with new entries

--- docs/edit/official-clubs.js
+++ docs/edit/official-clubs.js
@@ -27,16 +27,40 @@ const officialClubsData = [
   {
     category: "종교", // 카테고리 필터용 (officialClubsConfig.categories 중 하나)
     image: "image/정규동아리/엘피스/엘피스.jpg", // 대표 이미지 경로
-    subImage1: "docs/image/정규동아리/엘피스/엘피스 2.png", // 서브 이미지 1 (없으면 null)
+    subImage1: "image/정규동아리/엘피스/엘피스 2.png", // 서브 이미지 1 (없으면 null)
     subImage2: null, // 서브 이미지 2 (없으면 null)
     title: "엘피스",
     description: "엘피스는 기독교정신을 바탕으로\n 예수 그리스도의 사랑과 실천으로 공동체를 이루며 그 시대의문화에 맞춰 어느 곳에서든지 복음을 전하는 데 목적을 두고 있습니다! \n 주된 활동은 학교 채플과 신앙수련회 찬양팀 방중사역 (수련회, 고교채플) 등이 있습니다!",
-    detail: "엘피스는 서울신학대학교 소속 찬양선교동아리입니다.\n 매주 정기 연습과 합주를 통해 예배를 준비하며,\n학기 중 채플 찬양 인도와 각종 교내외 행사에서 활발히 활동하고 있습니다.", // 모달 팝업에 표시될 상세 설명
+    detail: null, // 모달 팝업에 표시될 상세 설명
     kakaoLink: null, // 카카오톡 오픈채팅 링크 (없으면 null)
     googleFormLink: "https://forms.gle/example-elpeace", // 구글폼 링크 (없으면 null)
-    qrCodeImage: "image/정규동아리/엘피스/테스트QR코드.png", // QR 코드 이미지 경로 (없으면 null)
+    qrCodeImage: null, // QR 코드 이미지 경로 (없으면 null)
     contact: "010-9552-8018", // 연락처 (없으면 null)
     instagram: "https://instagram.com/_elpeace_", // 인스타그램 링크 (없으면 null)
+    facebook: null, // 페이스북 링크 (없으면 null)
+  },
+  // #####################
+  
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "종교", // 카테고리 필터용 (officialClubsConfig.categories 중 하나)
+    image: "image/정규동아리/엘피스/엘피스.jpg", // 대표 이미지 경로
+    subImage1: "image/정규동아리/엘피스/엘피스 2.png", // 서브 이미지 1 (없으면 null)
+    subImage2: null, // 서브 이미지 2 (없으면 null)
+    title: "CCC",
+    description: "서울신학대학교 중앙동아리 CCC는
+      전국 41개 도시, 320여 개 대학에서 약 2만 명의 대학생들이 함께하는 전국 연합 기독교 동아리입니다.
+“Movements Everywhere”라는 비전 아래
+성령의 능력으로 그리스도를 전하고 제자를 세우며,
+1:1 순모임, 캠퍼스 모임, 연합 채플, 수련회와 단기선교 등
+다양한 활동을 통해 신앙과 공동체의 성장을 경험할 수 있는 동아리입니다.
+",
+    detail: null, // 모달 팝업에 표시될 상세 설명
+    kakaoLink: null, // 카카오톡 오픈채팅 링크 (없으면 null)
+    googleFormLink: "https://forms.gle/FAiHWku5BQAqffDN8", // 구글폼 링크 (없으면 null)
+    qrCodeImage: null, // QR 코드 이미지 경로 (없으면 null)
+    contact: "010-7467-5623", // 연락처 (없으면 null)
+    instagram: null, // 인스타그램 링크 (없으면 null)
     facebook: null, // 페이스북 링크 (없으면 null)
   },
   // #####################
