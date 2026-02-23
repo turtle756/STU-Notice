6278d136013f5a444976efbae61a2a804705be5a
stuseed42-beep <stuseed42@gmail.com>
2026-02-23 13:27:37 +0900

    Update partners.js

--- docs/edit/partners.js
+++ docs/edit/partners.js
@@ -73,31 +73,31 @@ const partnersData = [
   {
     category: "카페",
     image: "https://picsum.photos/400/300?2",
-    title: "캠퍼스 카페",
-    description: "음료 전 메뉴 500원 할인",
+    title: "셀렉토 커피",
+    description: "학생증 체줄 시 제조음료 10% 할",
     subImage1: null,
     subImage2: null,
     location: "학교 내 학생회관 1층",
     discount: "500원 할인",
     mapCodeModal: `<!-- * 카카오맵 - 지도퍼가기 -->
 <!-- 1. 지도 노드 -->
-<div id="daumRoughmapContainer1766559121887" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+<div id="daumRoughmapContainer1771820707204" class="root_daum_roughmap root_daum_roughmap_landing"></div>
 
 <!--
-        2. 설치 스크립트
-        * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
+	2. 설치 스크립트
+	* 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
 -->
 <script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
 
 <!-- 3. 실행 스크립트 -->
 <script charset="UTF-8">
-  new daum.roughmap.Lander({
-    "timestamp" : "1766559121887",
-    "key" : "esqd4navtin",
-    "mapWidth" : "560",
-    "mapHeight" : "300"
-  }).render();
-</script>`,
+	new daum.roughmap.Lander({
+		"timestamp" : "1771820707204",
+		"key" : "hujiy4pjwe8",
+		"mapWidth" : "560",
+		"mapHeight" : "300"
+	}).render();
+</script>
   },
   // #####################
 
