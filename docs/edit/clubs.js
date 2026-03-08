1f175de799ab8c5d163272b835045ffb648c9603
김기현 <131948733+turtle756@users.noreply.github.com>
2026-03-09 00:44:03 +0900

    [관리자] 콘텐츠 업데이트 - 2026. 3. 8. 오후 3:44:03

--- docs/edit/clubs.js
+++ docs/edit/clubs.js
@@ -7,53 +7,62 @@
 const clubsData = [
   // @@@@@@@@@@@@@@@@@
   {
-    category: "스포츠", // 카테고리 필터용 (clubsConfig.categories 중 하나)
-    image: "image/자율동아리&소모임/축구동아리/축구동아리.png", // 카드 이미지 경로
+    category: "스포츠",
+    image: "image/자율동아리&소모임/축구동아리/축구동아리.png",
+    subImage1: null,
+    subImage2: null,
     title: "(예시)축구 동아리",
-    description: `함께 축구하며 친목을 다지는 동아리입니다.`,
-    kakaoLink: "https://open.kakao.com/o/example1", // 카카오톡 오픈채팅 링크 (가입 문의용)
-    detail: `매주 화/목요일 저녁 7시에 체육관에서 만나 축구를 즐깁니다. 초보자도 환영하며, 학기별로 1회 대학 연합 친선 경기를 개최합니다. 회비는 월 1만원이며, 유니폼과 음료 구매에 사용됩니다.`, // 모달 팝업에 표시될 상세 설명
+    description: "함께 축구하며 친목을 다지는 동아리입니다.",
+    kakaoLink: "https://open.kakao.com/o/example1",
+    detail: "매주 화/목요일 저녁 7시에 체육관에서 만나 축구를 즐깁니다. 초보자도 환영하며, 학기별로 1회 대학 연합 친선 경기를 개최합니다. 회비는 월 1만원이며, 유니폼과 음료 구매에 사용됩니다.",
   },
   // #####################
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "학술", // 카테고리 필터용 (clubsConfig.categories 중 하나)
-    image: "image/자율동아리&소모임/코딩스터디/코딩스터디.png", // 카드 이미지 경로
+    category: "학술",
+    image: "image/자율동아리&소모임/코딩스터디/코딩스터디.png",
+    subImage1: null,
+    subImage2: null,
     title: "(예시)코딩 스터디",
-    description: `알고리즘 문제풀이 및 프로젝트 스터디`,
-    kakaoLink: "https://open.kakao.com/o/example3", // 카카오톡 오픈채팅 링크 (가입 문의용)
-    detail: ``, // 모달 팝업에 표시될 상세 설명
+    description: "알고리즘 문제풀이 및 프로젝트 스터디",
+    kakaoLink: "https://open.kakao.com/o/example3",
+    detail: "",
   },
   // #####################
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "문화예술", // 카테고리 필터용 (clubsConfig.categories 중 하나)
-    image: "image/자율동아리&소모임/밴드동아리/밴드동아리.png", // 카드 이미지 경로
+    category: "문화예술",
+    image: "image/자율동아리&소모임/밴드동아리/밴드동아리.png",
+    subImage1: null,
+    subImage2: null,
     title: "(예시)밴드 동아리",
-    description: `다양한 악기로 밴드를 구성해 공연합니다.`,
-    kakaoLink: "https://open.kakao.com/o/example4", // 카카오톡 오픈채팅 링크 (가입 문의용)
-    detail: ``, // 모달 팝업에 표시될 상세 설명
+    description: "다양한 악기로 밴드를 구성해 공연합니다.",
+    kakaoLink: "https://open.kakao.com/o/example4",
+    detail: "",
   },
   // #####################
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "학술", // 카테고리 필터용 (clubsConfig.categories 중 하나)
-    image: "image/자율동아리&소모임/독서토론/독서토론.png", // 카드 이미지 경로
+    category: "학술",
+    image: "image/자율동아리&소모임/독서토론/독서토론.png",
+    subImage1: null,
+    subImage2: null,
     title: "(예시)독서 토론 모임",
-    description: `매주 한 권의 책을 읽고 토론합니다.`,
-    kakaoLink: "https://open.kakao.com/o/example5", // 카카오톡 오픈채팅 링크 (가입 문의용)
-    detail: ``, // 모달 팝업에 표시될 상세 설명
+    description: "매주 한 권의 책을 읽고 토론합니다.",
+    kakaoLink: "https://open.kakao.com/o/example5",
+    detail: "",
   },
   // #####################
+
 ];
 
 const clubsConfig = {
-  pageTitle: "자율 동아리 & 소모임", // 페이지 상단 제목
-  pageSubtitle: "관심사가 같은 친구들과 함께하세요!", // 페이지 상단 부제목
-  applyFormLink: "https://forms.gle/3t7igzSKnyMeLT1n8", // 페이지 상단 "신청하기" 버튼 링크
-  itemsPerPage: 15, // 한 페이지에 표시할 카드 수
-  categories: ["스포츠", "학술", "문화예술", "취미"] // 필터 버튼 목록 (추가/삭제 가능)
+  pageTitle: "자율 동아리 & 소모임",
+  pageSubtitle: "관심사가 같은 친구들과 함께하세요!",
+  applyFormLink: "https://forms.gle/3t7igzSKnyMeLT1n8",
+  itemsPerPage: 15,
+  categories: ["스포츠","학술","문화예술","취미","수다방"],
 };
--- docs/edit/partners.js
+++ docs/edit/partners.js
@@ -7,38 +7,6 @@
 const partnersData = [
   // @@@@@@@@@@@@@@@@@
   {
-    category: "음식",
-    image: "image/제휴 업체/홍대개미/1.png",
-    title: "홍대개미",
-    description: "학생증 제시 시 전 메뉴 10% 할인",
-    subImage1: "image/제휴 업체/홍대개미/2.png",
-    subImage2: "image/제휴 업체/홍대개미/3.png",
-    location: "경기 부천시 원미구 부천로 9번길 24",
-    discount: "10% 할인",
-    mapCodeModal: `<!-- * 카카오맵 - 지도퍼가기 -->
-<!-- 1. 지도 노드 -->
-<div id="daumRoughmapContainer1771576074785" class="root_daum_roughmap root_daum_roughmap_landing"></div>
-
-<!--
-        2. 설치 스크립트
-        * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
--->
-<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
-
-<!-- 3. 실행 스크립트 -->
-<script charset="UTF-8">
-        new daum.roughmap.Lander({
-                "timestamp" : "1771576074785",
-                "key" : "hpb9fgj23y3",
-                "mapWidth" : "560",
-                "mapHeight" : "300"
-        }).render();
-</script>`,
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
     category: "기타",
     image: "image/제휴 업체/학생화방문구센터/1.png",
     title: "학생화방문구센터",
@@ -71,17 +39,51 @@ const partnersData = [
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "카페",
-    image: "image/제휴 업체/캠퍼스 카페/1.png",
-    title: "캠퍼스 카페",
-    description: "음료 전 메뉴 500원 할인",
-    subImage1: "image/제휴 업체/캠퍼스 카페/2.png",
-    subImage2: "image/제휴 업체/캠퍼스 카페/3.png",
-    location: "학교 내 학생회관 1층",
-    discount: "500원 할인",
+    category: "음식",
+    image: "image/제휴 업체/홍대개미/1.png",
+    title: "홍대개미",
+    description: "학생증 제시 시 전 메뉴 10% 할인",
+    subImage1: "image/제휴 업체/홍대개미/2.png",
+    subImage2: "image/제휴 업체/홍대개미/3.png",
+    location: "경기 부천시 원미구 부천로 9번길 24",
+    discount: "10% 할인",
     mapCodeModal: `<!-- * 카카오맵 - 지도퍼가기 -->
 <!-- 1. 지도 노드 -->
-<div id="daumRoughmapContainer1772371531344" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+<div id="daumRoughmapContainer1771576074785" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+
+<!--
+        2. 설치 스크립트
+        * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
+-->
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
+
+<!-- 3. 실행 스크립트 -->
+<script charset="UTF-8">
+        new daum.roughmap.Lander({
+                "timestamp" : "1771576074785",
+                "key" : "hpb9fgj23y3",
+                "mapWidth" : "560",
+                "mapHeight" : "300"
+        }).render();
+</script>`,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "카페",
+    image: "image/제휴 업체/프루미에 케이크/1.png",
+    title: "프루미에 케이크",
+    description: `미니케이크 1000원 할인
+1호, 2호 케이크 2000원 할인
+네이버 영수증 리뷰 작성 시 디자인초 제공`,
+    subImage1: "image/제휴 업체/프루미에 케이크/2.png",
+    subImage2: "image/제휴 업체/프루미에 케이크/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: `<!-- * 카카오맵 - 지도퍼가기 -->
+<!-- 1. 지도 노드 -->
+<div id="daumRoughmapContainer1772982802334" class="root_daum_roughmap root_daum_roughmap_landing"></div>
 
 <!--
 	2. 설치 스크립트
@@ -92,12 +94,184 @@ const partnersData = [
 <!-- 3. 실행 스크립트 -->
 <script charset="UTF-8">
 	new daum.roughmap.Lander({
-		"timestamp" : "1772371531344",
-		"key" : "im26qtm88an",
+		"timestamp" : "1772982802334",
+		"key" : "immgvs7e9jw",
 		"mapWidth" : "560",
 		"mapHeight" : "300"
 	}).render();
 </script>`,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "카페",
+    image: "image/제휴 업체/체리콩/1.png",
+    title: "체리콩",
+    description: "음료 전 메뉴 500원 할인",
+    subImage1: "image/제휴 업체/체리콩/2.png",
+    subImage2: "image/제휴 업체/체리콩/3.png",
+    location: "",
+    discount: "",
+    mapCodeModal: `<!-- * 카카오맵 - 지도퍼가기 -->
+<!-- 1. 지도 노드 -->
+<div id="daumRoughmapContainer1772983665682" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+
+<!--
+	2. 설치 스크립트
+	* 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
+-->
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
+
+<!-- 3. 실행 스크립트 -->
+<script charset="UTF-8">
+	new daum.roughmap.Lander({
+		"timestamp" : "1772983665682",
+		"key" : "ixtdxcwzqzx",
+		"mapWidth" : "560",
+		"mapHeight" : "300"
+	}).render();
+</script>`,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "문화",
+    image: "image/제휴 업체/더홀릭 보드게임/1.png",
+    title: "더홀릭 보드게임 카페",
+    description: null,
+    subImage1: "image/제휴 업체/더홀릭 보드게임/2.png",
+    subImage2: "image/제휴 업체/더홀릭 보드게임/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "문화",
+    image: "image/제휴 업체/비원비 스트라이크/1.png",
+    title: "비원비 스트라이크 볼링",
+    description: null,
+    subImage1: "image/제휴 업체/비원비 스트라이크/2.png",
+    subImage2: "image/제휴 업체/비원비 스트라이크/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "의료",
+    image: "image/제휴 업체/삼성 미라클안과/1.png",
+    title: "삼성 미라클 안과",
+    description: null,
+    subImage1: "image/제휴 업체/삼성 미라클안과/4.png",
+    subImage2: "image/제휴 업체/삼성 미라클안과/5.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "교육",
+    image: "image/제휴 업체/시원스쿨/1.png",
+    title: "시원스쿨",
+    description: null,
+    subImage1: "image/제휴 업체/시원스쿨/2.png",
+    subImage2: "image/제휴 업체/시원스쿨/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "음식",
+    image: "image/제휴 업체/신의주 찹쌀순대/1.png",
+    title: "신의주 찹쌀순대",
+    description: null,
+    subImage1: "image/제휴 업체/신의주 찹쌀순대/2.png",
+    subImage2: "image/제휴 업체/신의주 찹쌀순대/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "카페",
+    image: "image/제휴 업체/이디야 커피/1.png",
+    title: "이디야 커피",
+    description: null,
+    subImage1: "image/제휴 업체/이디야 커피/2.png",
+    subImage2: null,
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "음식",
+    image: "image/제휴 업체/카츠돼왕+쓰리퍼 쌀국수/1.png",
+    title: "카츠돼왕 + 쓰리퍼 쌀국수",
+    description: null,
+    subImage1: "image/제휴 업체/카츠돼왕+쓰리퍼 쌀국수/2.png",
+    subImage2: "image/제휴 업체/카츠돼왕+쓰리퍼 쌀국수/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "음식",
+    image: "image/제휴 업체/하늘식당/1.png",
+    title: "하늘식당",
+    description: null,
+    subImage1: "image/제휴 업체/하늘식당/2.png",
+    subImage2: "image/제휴 업체/하늘식당/3.png",
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "교육",
+    image: "image/제휴 업체/한국인터넷교육방송/1.png",
+    title: "한국인터넷교육방송",
+    description: null,
+    subImage1: "image/제휴 업체/한국인터넷교육방송/2.png",
+    subImage2: null,
+    location: null,
+    discount: null,
+    mapCodeModal: null,
+  },
+  // #####################
+
+  // @@@@@@@@@@@@@@@@@
+  {
+    category: "교육",
+    image: "image/제휴 업체/YBM/1.png",
+    title: "YBM",
+    description: null,
+    subImage1: "image/제휴 업체/YBM/2.png",
+    subImage2: null,
+    location: null,
+    discount: null,
+    mapCodeModal: null,
   },
   // #####################
 
@@ -108,5 +282,5 @@ const partnersConfig = {
   pageSubtitle: "학생증을 제시하고 다양한 혜택을 받으세요!",
   suggestFormLink: "https://forms.gle/partner-suggest",
   itemsPerPage: 15,
-  categories: ["음식","카페","문화","기타"],
+  categories: ["음식","카페","문화","의료","교육"],
 };
