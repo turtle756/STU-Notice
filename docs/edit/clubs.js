e45a5bdd97ff261b95620bec1b91ad3e19a6b6e2
김기현 <131948733+turtle756@users.noreply.github.com>
2026-03-09 01:40:21 +0900

    [관리자] 콘텐츠 업데이트 - 2026. 3. 8. 오후 4:40:21

--- docs/edit/clubs.js
+++ docs/edit/clubs.js
@@ -61,7 +61,7 @@ const clubsData = [
 
 const clubsConfig = {
   pageTitle: "자율 동아리 & 소모임",
-  pageSubtitle: "관심사가 같은 친구들과 함께하세요!",
+  pageSubtitle: "관심사가 같은 학우들과 자유롭게 소통하세요!",
   applyFormLink: "https://forms.gle/3t7igzSKnyMeLT1n8",
   itemsPerPage: 15,
   categories: ["스포츠","학술","문화예술","취미","수다방"],
