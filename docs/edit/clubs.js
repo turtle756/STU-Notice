a9022556c6a636e5397b51d5c300924c842e5e5a
김기현 <131948733+turtle756@users.noreply.github.com>
2026-03-23 18:18:21 +0900

    [관리자] 콘텐츠 업데이트 - 2026. 3. 23. AM 9:18:21

--- docs/edit/clubs.js
+++ docs/edit/clubs.js
@@ -7,53 +7,18 @@
 const clubsData = [
   // @@@@@@@@@@@@@@@@@
   {
-    category: "스포츠",
-    image: "image/자율동아리&소모임/축구동아리/축구동아리.png",
+    category: null,
+    image: "image/자율동아리&소모임/풋살동아리.png",
     subImage1: null,
     subImage2: null,
-    title: "(예시)축구 동아리",
-    description: "함께 축구하며 친목을 다지는 동아리입니다.",
-    kakaoLink: "https://open.kakao.com/o/example1",
-    detail: "매주 화/목요일 저녁 7시에 체육관에서 만나 축구를 즐깁니다. 초보자도 환영하며, 학기별로 1회 대학 연합 친선 경기를 개최합니다. 회비는 월 1만원이며, 유니폼과 음료 구매에 사용됩니다.",
-  },
-  // #####################
+    title: "골 때리는 서신인 ",
+    description: "풋살을 좋아하는 모든 재학생들을 모집합니다!",
+    kakaoLink: "https://open.kakao.com/o/smOzUQmi",
+    detail: `꾸준한 체육활동 참여로 건강한 커뮤니티를 이루고 싶으신 분
+열정적인 참여로 활동에 임할 수 있으신 분
+풋살을 즐기고 열린 마음으로 팀원들과 함께 협력하는 분!
 
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "학술",
-    image: "image/자율동아리&소모임/코딩스터디/코딩스터디.png",
-    subImage1: null,
-    subImage2: null,
-    title: "(예시)코딩 스터디",
-    description: "알고리즘 문제풀이 및 프로젝트 스터디",
-    kakaoLink: "https://open.kakao.com/o/example3",
-    detail: "",
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "문화예술",
-    image: "image/자율동아리&소모임/밴드동아리/밴드동아리.png",
-    subImage1: null,
-    subImage2: null,
-    title: "(예시)밴드 동아리",
-    description: "다양한 악기로 밴드를 구성해 공연합니다.",
-    kakaoLink: "https://open.kakao.com/o/example4",
-    detail: "",
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "학술",
-    image: "image/자율동아리&소모임/독서토론/독서토론.png",
-    subImage1: null,
-    subImage2: null,
-    title: "(예시)독서 토론 모임",
-    description: "매주 한 권의 책을 읽고 토론합니다.",
-    kakaoLink: "https://open.kakao.com/o/example5",
-    detail: "",
+함께 즐겁게 활동하실 분들을 모집합니다!`,
   },
   // #####################
 
--- docs/edit/events.js
+++ docs/edit/events.js
@@ -48,39 +48,6 @@ const eventsData = [
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "행사",
-    image: "image/행사&공모전/힐송 컨퍼런스.jpg",
-    subImage1: null,
-    subImage2: null,
-    title: "힐송 컨퍼런스",
-    date: "2026.03.26(목)",
-    organizer: "서울신학대학교",
-    location: "성결인의 집 ",
-    description: `서울신학대학교와 HILLSONG COLLEGE가
-2024년부터 협약을 맺어 "LEAD WORSHIPER" 교육 과정을
-운영하고 있습니다.
-
-오는 3월 26일, 한국의 모든 예배자들과 함께
-힐송의 탁월한 예배 사역을 나누고 배우는 자리에 초청합니다.
-
-"2026 STU-HILLSONG COLLEGE 예배찬양컨퍼런스"를 통해
-하나님이 원하시고 기뻐하시는 예배를 함께 세워가기를 바랍니다.
-(누구나 신청할 수 있는 오픈된 컨퍼런스입니다.)
-
-교회와 공동체 그리고 주변에 널리 소식을 알려주시면
-감사드리겠습니다.`,
-    link: "https://2026stuhillsong.my.canva.site/c267ygc42f0ppzwh",
-    applyLink: "https://docs.google.com/forms/d/e/1FAIpQLScTKEuuL52ILXHMgxyN8nYRsLUIT3upjuroGzgMjCGbE1x3Hw/viewform",
-    details: {
-      target: " 한국의 모든 예배자",
-      contact: "서울신학대학교 교학과 (032-340-9265)",
-      benefits: "재학생 저녁7시 집회 참여시 채플 마스터카드(채플1회 결석권) 배부"
-    },
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
     category: "안내사항",
     image: "image/행사&공모전/KakaoTalk_20260311_105709679.jpg",
     subImage1: "image/행사&공모전/KakaoTalk_20260311_105709679_01.jpg",
@@ -113,215 +80,33 @@ const eventsData = [
 
   // @@@@@@@@@@@@@@@@@
   {
-    category: "프로젝트",
-    image: "image/행사&공모전/교양교육원 학생 모니터링단.jpg",
-    subImage1: null,
-    subImage2: null,
-    title: "교양교육원 학생 모니터링단 ",
-    date: null,
-    organizer: "교양교육원",
-    location: null,
-    description: `[교양교육원 2026-1학기 학생모니터링단 모집 안내]
-
-
-안녕하세요, 교양교육원 STU교양교육연구센터입니다.
-2026-1학기 교양교육 학생모니터링단을 모집합니다:)
-
-📌참여대상​   본교 학부 재학생
-
-📌선발인원​   총 6명
-
-📌신청기간   3/12(목)~3/22(일)까지
-*내부 사정에 의해 변경될 수 있음
-
-📌신청방법​   신청서 및 개인정보 수집 이용 동의서 작성-> e-Room 제출
-
-📌활동기간   26년 3월~7월
-*선발 확정 후  O.T  필수 참석
-
-📌활동내용
--교양교육 설문 제작 및 결과보고서 작성
-
-📌모니터링단 혜택
- - 위촉장 수여
- - 장학금 (활동시간에 따라 차등지급)
-
-많은 관심과 참여 부탁드립니다 :)
-감사합니다 :)`,
-    link: null,
-    applyLink: "https://eroom.stu.ac.kr?djkSNAr",
-    details: {
-      contact: "교양교육원 STU교양교육연구센터 032-340-9239"
-    },
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: null,
-    image: "image/행사&공모전/KakaoTalk_20260311_171053167.png",
-    subImage1: null,
-    subImage2: null,
-    title: "이음멘토링 멘티모집",
-    date: "3월 15일까지 신청",
-    organizer: "학생상담센터",
-    location: null,
-    description: `[26-1 이음멘토링 멘티 추가 모집]
-
-안녕하세요 학생상담센터입니다.
-
-26-1학기 이음멘토링 신·편입생 멘티를 추가모집합니다. 이음멘토링을 통해 선배멘토와 만나 학교생활 및 성적관리 방법, 진로고민 등을 나눌 수 있습니다. 처음 시작하는 대학생활, 어떻게 해야 할 지 걱정된다면, 이음멘토링 멘티에 참여해보세요!
-
-✔ 대상 : 신·편입생 
-✔ 신청기간 : ~3월15일까지⭐
-✔ 활동기간 : 26-1학기
-✔ 혜택 : 마일리지 3만점
-✔ 자세한 안내는 포스터를 참고해주시기 바랍니다. `,
-    link: null,
-    applyLink: "https://eroom.stu.ac.kr?mdlCxZxh0",
-    details: {
-      contact: "학생상담센터(032-340-9380)",
-      benefits: "마일리지 3만점"
-    },
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "프로젝트",
-    image: "image/행사&공모전/한국장학재단 멘토링 사업.png",
-    subImage1: null,
-    subImage2: null,
-    title: "한국장학재단 멘토링사업",
-    date: null,
-    organizer: "주최: 다누리컴버시티센터",
-    location: null,
-    description: `🏡한국장학재단 멘토링 사업 사전 홍보 안내🏡
-
-안녕하세요 다누리컴버시티센터입니다.
-“교육의 가치” 를 나누며 장학금도 받고,
-의미 있는 사회활동을 하고 싶지 않으신가요?
-
-다누리컴버시티센터에서는 한국장학재단 대학생청소년교육지원사업과 다문화•탈북학생 멘토링 사업를 운영하고 있습니다. 
-📚학업 향상을 위해 도와주고
-🙋🏻‍♀️학교 생활 적응을 위해 이야기도 나눠주고
-🍀함께 성장할 수 있는 기회!
-
-멘티들을 향한 사랑만 있다면 누구나 참여할 수 있는 사업! 
-교육적인 가치를 나누며 장학금도 받을 수 있는 기회 절대 놓치지 마세요! 자세한 내용은 카드뉴스를 참고하시고,
-많은 관심 부탁드립니다 :) 
-⭐️신청기간은 다음주~3월초 모집 예정이며 추가 공지 예정)
-📞 문의 : 다누리컴버시티센터(032-340-9347)`,
-    link: null,
-    applyLink: null,
-    details: {},
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
     category: "행사",
-    image: "image/행사&공모전/아나바나 플리마켓.png",
+    image: "image/행사&공모전/힐송 컨퍼런스.jpg",
     subImage1: null,
     subImage2: null,
-    title: "아나바다 플리마켓",
-    date: "2026.03.05(목)~2026.03.20(금)",
-    organizer: "GREENUS(그린어스)",
-    location: null,
-    description: `♻️아나바다 플리마켓♻️
+    title: "힐송 컨퍼런스",
+    date: "2026.03.26(목)",
+    organizer: "서울신학대학교",
+    location: "성결인의 집 ",
+    description: `서울신학대학교와 HILLSONG COLLEGE가
+2024년부터 협약을 맺어 "LEAD WORSHIPER" 교육 과정을
+운영하고 있습니다.
 
-GREENUS 동아리와 호태원에서 2026 아나바다 플리마켓을 진행합니다! 
+오는 3월 26일, 한국의 모든 예배자들과 함께
+힐송의 탁월한 예배 사역을 나누고 배우는 자리에 초청합니다.
 
-아나바다 플리마켓은 학우들이 더 이상 사용하지 않는 옷과 물품을 기부하면, 이를 필요로 하는 다른 학우들에게 다시 전달해 물건이 재사용될 수 있도록 하는 나눔 행사입니다.
+"2026 STU-HILLSONG COLLEGE 예배찬양컨퍼런스"를 통해
+하나님이 원하시고 기뻐하시는 예배를 함께 세워가기를 바랍니다.
+(누구나 신청할 수 있는 오픈된 컨퍼런스입니다.)
 
-모집 기간: 2026.03.05(목)~2026.03.20(금) 
-모집 대상: 서울신학대학교 재학생
-
-버리기엔 아깝고, 누군가에겐 소중한 물건.
-내가 쓰지 않는 물건을 기부하며 자원순환을 실천하고, 작은 나눔으로 따뜻한 마음을 전해보세요!!
-
-안 입는 옷, 사용하지 않는 물건 기부 가능 O
-
-(* 기부해주신 물품은 아나바다 플리마켓에서 다른 물품으로 교환하실 수 있습니다)
-
-학우 여러분의 많은 참여 바랍니다!`,
-    link: null,
-    applyLink: null,
+교회와 공동체 그리고 주변에 널리 소식을 알려주시면
+감사드리겠습니다.`,
+    link: "https://2026stuhillsong.my.canva.site/c267ygc42f0ppzwh",
+    applyLink: "https://docs.google.com/forms/d/e/1FAIpQLScTKEuuL52ILXHMgxyN8nYRsLUIT3upjuroGzgMjCGbE1x3Hw/viewform",
     details: {
-      contact: "문의 : 인스타그램 stu_greenus",
-      target: "서울신학대학교 재학생"
-    },
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "프로젝트",
-    image: "image/행사&공모전/인권 서포터즈.png",
-    subImage1: null,
-    subImage2: null,
-    title: "대학인권 서포터즈",
-    date: "2026년 3월 9일 - 2026년 3월 16일",
-    organizer: "대학인권센터",
-    location: null,
-    description: `[2026-1학기 대학인권센터 서포터즈 장학생 모집]
-안녕하세요. 대학인권센터입니다.
-2026학년도 1학기 동안 함께 활동할 인권서포터즈 장학생을 모집합니다.
-
-★활동내용
-- O.T 3월 중 예상
-- 인권 캠페인 및 인권 교육 참여, 행사 지원 등
--활동 기록 및 피드백 공유
-
-★활동혜택
-- 활동 장학금 
-※ 장학금 지급을 위한 서류 제출 필수
-
-★신청기간
-2026년 3월 9일 - 2026년 3월 16일
-
-★신청방법
-1. QR접속 or 이룸 링크 접속
-https://eroom.stu.ac.kr?R8XSOXRt9
-2. 신청서 작성 후 첨부하여 제출
-3. 면접 대상자 개별 안내`,
-    link: null,
-    applyLink: "https://eroom.stu.ac.kr/Community/Program/ProgramView.aspx?pgdx=0A264A004CD3FEFFF4B00B42DFF7CC919A30B61ECEFEEDDBD841D6A12249825A&subNum=1",
-    details: {
-      contact: "대학인권센터 032-340-9453",
-      benefits: "- 활동 장학금  ※ 장학금 지급을 위한 서류 제출 필수"
-    },
-  },
-  // #####################
-
-  // @@@@@@@@@@@@@@@@@
-  {
-    category: "프로젝트",
-    image: "image/행사&공모전/학습 WE함 공동체.jpg",
-    subImage1: null,
-    subImage2: null,
-    title: "학습 WE함 공동체",
-    date: "모집기간: 02/26(목) ~ 3/20(금)",
-    organizer: "교수학습개발센터",
-    location: null,
-    description: `[2026-1학기 교수학습개발센터 공동체 학습We함(+또래) 모집 안내]
-안녕하세요. 교수학습개발센터입니다. 2026학년도 1학기 공동체 학습We함 참여자를 모집하고 있습니다.
-공동체 학습We함은 학습에 “공동목표”가 있는 학생들이 3-5명 팀을 이루어 학습하는 프로그램으로 전공, 자격증 등 다양한 분야에서 참여할 수 있습니다. 자세한 내용은 이룸에서 확인 가능하며, 재학생 분들의 많은 관심과 참여 부탁드립니다^^
- 
-▶ 모집기간: 02/26(목) ~ 3/20(금)
-▶ 참여혜택: 활동 종료 이후 심사를 통한 장학금 지급
-(최우수 50만원 1팀, 우수 30만원 N팀, 장려 15만원 N팀, 성실 10만원 N팀) 
-- 참여 완료자 전체 STU 마일리지 30,000점 지급
-▶ O T 안내: 3월 26일(금), 오후 12시 예정, Zoom
-▶ 신청링크: https://eroom.stu.ac.kr?8qbSwLJiQ
-
-많은 관심과 참여 부탁드립니다.
-감사합니다 :)`,
-    link: null,
-    applyLink: "https://eroom.stu.ac.kr/Community/Program/ProgramView.aspx?pgdx=7E3152486315A03A930D05811CECDD62A8AE5A1327B0FDE14C243A8AB363C570&subNum=1",
-    details: {
-      benefits: "활동 종료 이후 심사를 통한 장학금 지급 (최우수 50만원 1팀, 우수 30만원 N팀, 장려 15만원 N팀, 성실 10만원 N팀)  - 참여 완료자 전체 STU 마일리지 30,000점 지급",
-      contact: "교수학습개발센터 032-340-9150"
+      target: " 한국의 모든 예배자",
+      contact: "서울신학대학교 교학과 (032-340-9265)",
+      benefits: "재학생 저녁7시 집회 참여시 채플 마스터카드(채플1회 결석권) 배부"
     },
   },
   // #####################
